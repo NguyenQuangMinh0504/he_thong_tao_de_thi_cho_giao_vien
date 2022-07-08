9e6be8fdf5ca6202940f98e5afb77f45dbd1a732
Nguyễn Quang Minh <ngquangminh05042001@gmail.com>
2022-07-08 10:26:11 +0700

    add import data button to gui

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 from PyQt5 import QtWidgets
 
-from gui_main_screen.main_screen import Ui_giao_dien_chinh_frame
+from gui_main_screen.main_screen import Ui_main_screen_frame
 
 if __name__ == '__main__':
     import sys
     app = QtWidgets.QApplication(sys.argv)
     Frame = QtWidgets.QFrame()
-    ui = Ui_giao_dien_chinh_frame()
+    ui = Ui_main_screen_frame()
     ui.setupUi(Frame)
     Frame.show()
     sys.exit(app.exec_())
